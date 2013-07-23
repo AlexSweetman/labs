b2330f8a3da073083e61ad79d4afa68e4ad591a1
sweal144 <sweal144@sb303-17.otago.ac.nz>
2013-07-23 14:49:09 +1200

    Copied domain and GUI files from lab 2

---
+++ src/domain/Student.java
@@ -0,0 +1,42 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package domain;
+
+/**
+ *
+ * @author sweal144
+ */
+public class Student {
+   private String name;
+   private int ID;
+
+   public Student(String name, int ID) {
+      this.name = name;
+      this.ID = ID;
+   }
+   
+   
+
+   public String getName() {
+      return name;
+   }
+
+   public void setName(String name) {
+      this.name = name;
+   }
+
+   public int getID() {
+      return ID;
+   }
+
+   public void setID(int ID) {
+      this.ID = ID;
+   }
+
+   @Override
+   public String toString() {
+      return "Student{" + "name=" + name + ", ID=" + ID + '}';
+   }
+}
